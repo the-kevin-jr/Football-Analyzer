732107d7b753969e86f5d74a4c9788b5dc0d6787
thebongy <rishit.bansal0@gmail.com>
2019-11-06 08:21:23 +0530

    feat: Season fixtures

--- routes/season.js
+++ routes/season.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Season = require("../models/season").Season;
 const League = require("../models/league").League;
 const Team = require("../models/team").Team;
-
+const Fixture = require("../models/fixture").Fixture;
 
 router.get("/", async function (req, res) {
     const seasons = await Season.aggregate([
@@ -48,11 +48,60 @@ router.get("/", async function (req, res) {
 });
 
 router.get("/:id", async (req, res) => {
-    console.log(req.params.id);
     const season = await Season.findOne({seasonID: req.params.id}).exec();
     const league = await League.findOne({leagueID: season.leagueID}).exec();
-    const teams = await Team.find({seasonID: season.seasonID});
-    res.render('seasons', {season, league, teams});
+    const teams = await Team.find({seasonID: season.seasonID}).exec();
+    const fixtures = await Fixture.aggregate([
+        {
+            $match: {
+                seasonID: season.seasonID
+            }
+        },
+        {
+            $lookup:
+            {
+                from: 'teams',
+                localField: 'team1',
+                foreignField: 'teamID',
+                as: 'team1Details'
+            },
+        },{
+            $lookup:
+            {
+                from: 'teams',
+                localField: 'team2',
+                foreignField: 'teamID',
+                as: 'team2Details'
+            },
+        },{
+            $project:
+            {
+                "fixtureID": 1,
+                "seasonID": 1,
+                "date":1,
+                "team1": 1,
+                "team2": 1,
+                "team1Score": 1,
+                "team2Score": 1,
+                "team1Details": {"$arrayElemAt":["$team1Details", 0]},
+                "team2Details": {"$arrayElemAt":["$team2Details", 0]}
+            },
+        },{
+            $project:
+            {
+                "seasonID": 1,
+                "fixtureID": 1,
+                "date":1,
+                "team1": 1,
+                "team2": 1,
+                "team1Score": 1,
+                "team2Score": 1,
+                "team1Name": "$team1Details.name",
+                "team2Name": "$team2Details.name"
+            }
+        }
+    ]).exec();
+    res.render('seasons', {season, league, teams, fixtures});
 });
 
 exports.router = router;
