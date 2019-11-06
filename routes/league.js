b75251a7996733cc0a3b18c1f2e400cfbcb68a42
thebongy <rishit.bansal0@gmail.com>
2019-11-06 10:04:37 +0530

    feat: Show league details

--- routes/league.js
+++ routes/league.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const router = express.Router();
 
 const League = require("../models/league").League;
+const Season = require("../models/season").Season;
 
 router.get("/", async function (req, res) {
     const leagues = await League.find({});
@@ -24,6 +25,12 @@ router.get("/", async function (req, res) {
         data: leagues
     };
     res.render('listModel', templateVals);
+});
+
+router.get("/:id", async (req, res) => {
+    const league = await League.findOne({leagueID: req.params.id}).exec();
+    const seasons = await Season.find({leagueID: req.params.id}).exec();
+    res.render('league', {league, seasons});
 });
 
 exports.router = router;
